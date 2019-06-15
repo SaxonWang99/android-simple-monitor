2ebd93e0de9eeceaba05b5221e438cd70cddd72b
SaxonWang99 <shihan.wang@sjsu.edu>
2019-06-15 09:09:53 -0700

    Initial Commit

---
+++ app/src/main/java/cloud/artik/example/simplemonitor/ArtikCloudSession.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cloud.artik.example.simplemonitor;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import cloud.artik.model.Acknowledgement;
+import cloud.artik.model.ActionOut;
+import cloud.artik.model.MessageOut;
+import cloud.artik.model.WebSocketError;
+import cloud.artik.websocket.ArtikCloudWebSocketCallback;
+import cloud.artik.websocket.FirehoseWebSocket;
+
+public class ArtikCloudSession {
+    private static final String TAG = ArtikCloudSession.class.getSimpleName();
+    private final static String DEVICE_ID = "9dca525b847442d18bfd1c6b1ef32320";
+    private final static String DEVICE_TOKEN = "05625cc3d38d44a488e1e2bbbd88d270";
+    private final static String DEVICE_NAME = "Combined Flame Detection and Temperature Sensor";
+    public static final String NOTIFICATION = "NOTIFICATION";
+
+    private static ArtikCloudSession ourInstance = new ArtikCloudSession();
+    private static Context ourContext;
+
+    public final static String WEBSOCKET_LIVE_ONOPEN =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONOPEN";
+    public final static String WEBSOCKET_LIVE_ONMSG =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONMSG";
+    public final static String WEBSOCKET_LIVE_ONCLOSE =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONCLOSE";
+    public final static String WEBSOCKET_LIVE_ONERROR =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONERROR";
+    public final static String SDID = "sdid";
+    public final static String DEVICE_DATA = "data";
+    public final static String TIMESTEP = "ts";
+
+
+    private FirehoseWebSocket mFirehoseWS = null; //  end point: /live
+
+    public static ArtikCloudSession getInstance() {
+        return ourInstance;
+    }
+
+    private ArtikCloudSession() {
+        // Do nothing
+    }
+
+    public void setContext(Context context) {ourContext = context;}
+
+
+    public String getDeviceID() {return DEVICE_ID;}
+    public String getDeviceName() {return DEVICE_NAME;}
+
+    public void connectFirehoseWS() {
+        createFirehoseWebsocket();
+        try {
+            mFirehoseWS.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes a websocket /live connection
+     */
+    public void disconnectFirehoseWS() {
+        if (mFirehoseWS != null) {
+            try {
+                mFirehoseWS.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mFirehoseWS = null;
+    }
+
+    private void createFirehoseWebsocket() {
+        try {
+            mFirehoseWS = new FirehoseWebSocket(DEVICE_TOKEN, DEVICE_ID, null, null, null, new ArtikCloudWebSocketCallback() {
+                @Override
+                public void onOpen(int i, String s) {
+                    Log.d(TAG, "FirehoseWebSocket: onOpen()");
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONOPEN);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onMessage(MessageOut messageOut) {
+                    Log.d(TAG, "FirehoseWebSocket: onMessage(" + messageOut.toString() + ")");
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONMSG);
+                    intent.putExtra(SDID, messageOut.getSdid());
+                    intent.putExtra(DEVICE_DATA, messageOut.getData().toString());
+                    intent.putExtra(TIMESTEP, messageOut.getTs().toString());
+                    intent.putExtra(NOTIFICATION, createNotification(ourContext));
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onAction(ActionOut actionOut) {
+                }
+
+                @Override
+                public void onAck(Acknowledgement acknowledgement) {
+                    Log.d(TAG, "FirehoseWebSocket::onAck: ");
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONCLOSE);
+                    intent.putExtra("error", "mFirehoseWS is closed. code: " + code + "; reason: " + reason);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onError(WebSocketError ex) {
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONERROR);
+                    intent.putExtra("error", "mFirehoseWS error: " + ex.getMessage());
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onPing(long timestamp) {
+                    Log.d(TAG, "FirehoseWebSocket::onPing: " + timestamp);
+                }
+            });
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Notification createNotification(Context ctx){
+        PendingIntent pi = PendingIntent.getActivity(ctx, 0, new Intent(ctx,MainActivity.class), 0);
+        Notification notification = new Notification.Builder(ctx)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Flame Monitor Notification")
+                .setContentText("There is a fire detected in your house.")
+                .setContentIntent(pi)
+                .setAutoCancel(false)
+                .build();
+
+        return notification;
+    }
+}
---
+++ app/src/main/java/cloud/artik/example/simplemonitor/MainActivity.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cloud.artik.example.simplemonitor;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class MainActivity  extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mLiveStatus;
+    private TextView mDeviceName;
+    private TextView mDeviceID;
+    private TextView mDeviceStatus;
+    private TextView mStatusUpdateTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLiveStatus = (TextView)findViewById(R.id.live_status);
+        mDeviceName = (TextView)findViewById(R.id.device_name);
+        mDeviceID = (TextView)findViewById(R.id.device_id);
+        mDeviceStatus = (TextView)findViewById(R.id.device_status);
+        mStatusUpdateTime = (TextView)findViewById(R.id.status_update_time);
+
+        setTitle(R.string.device_monitor_title);
+
+        ArtikCloudSession.getInstance().setContext(this);
+        mDeviceID.setText("Device ID: " + ArtikCloudSession.getInstance().getDeviceID());
+        mDeviceName.setText("Device Name: " + ArtikCloudSession.getInstance().getDeviceName());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mWSUpdateReceiver,
+                makeWebsocketUpdateIntentFilter());
+        mLiveStatus.setText("Connecting to /live");
+        ArtikCloudSession.getInstance().connectFirehoseWS();//non blocking
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mWSUpdateReceiver);
+        ArtikCloudSession.getInstance().disconnectFirehoseWS(); //non blocking
+    }
+
+
+    private static IntentFilter makeWebsocketUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONOPEN);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONMSG);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONCLOSE);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONERROR);
+        return intentFilter;
+    }
+
+    private final BroadcastReceiver mWSUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ArtikCloudSession.WEBSOCKET_LIVE_ONOPEN.equals(action)) {
+                displayLiveStatus("WebSocket /live connected");
+            } else if (ArtikCloudSession.WEBSOCKET_LIVE_ONMSG.equals(action)) {
+                String status = intent.getStringExtra(ArtikCloudSession.DEVICE_DATA);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = intent.getParcelableExtra(ArtikCloudSession.NOTIFICATION);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(status);
+                    String isOnFire = jsonObject.getString("onFire");
+                    if(!"false".equalsIgnoreCase(isOnFire))
+                        notificationManager.notify(0,notification);
+                }catch (JSONException e){
+                    Log.e(TAG, status);
+                }
+
+
+                String updateTime = intent.getStringExtra(ArtikCloudSession.TIMESTEP);
+                displayDeviceStatus(status, updateTime);
+            } else if (ArtikCloudSession.WEBSOCKET_LIVE_ONCLOSE.equals(action) ||
+                    ArtikCloudSession.WEBSOCKET_LIVE_ONERROR.equals(action)) {
+                displayLiveStatus(intent.getStringExtra("error"));
+            }
+        }
+    };
+
+    private void displayLiveStatus(String status) {
+        Log.d(TAG, status);
+        mLiveStatus.setText(status);
+    }
+
+    private void displayDeviceStatus(String status, String updateTimems) {
+        mDeviceStatus.setText(status);
+        long time_ms = Long.parseLong(updateTimems);
+        mStatusUpdateTime.setText(DateFormat.getDateTimeInstance().format(new Date(time_ms)));
+    }
+}
